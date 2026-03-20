4dc300b7c5390ba2f9deb2ff033ad5bf7f308a42
Victor Mochi <victormochi1@gmail.com>
2026-03-19 23:01:02 -0300

    feat: add decoder

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,8 @@ const Io = std.Io;
 
 const base64_encoder = @import("base64_encoder");
 
+const Base64Error = error{InvalidChar};
+
 const Base64 = struct {
     _table: *const [64]u8,
 
@@ -11,8 +13,24 @@ const Base64 = struct {
         return Base64{ ._table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" };
     }
 
-    pub fn char_at(self: Base64, index: u6) u8 {
+    pub fn char_at(self: Base64, index: usize) u8 {
         return self._table[index];
+    }
+
+    pub fn index_from_char(self: Base64, char: u8) Base64Error!u8 {
+        if (char == '=') {
+            return 64;
+        }
+
+        var output_index: u8 = 0;
+
+        for (self._table) |v| {
+            if (char == v) {
+                return output_index;
+            }
+            output_index += 1;
+        }
+        return Base64Error.InvalidChar;
     }
 };
 const _base64 = Base64.init();
@@ -22,13 +40,16 @@ const Encoder = struct {
         return Encoder{};
     }
 
-    // encode receives a slice of bytes another slice of u8s, but represented by chars
     pub fn encode(allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
         var list = try std.ArrayList(u8).initCapacity(allocator, ((input.len + 2) / 3) * 4);
 
         var i: usize = 0;
         var window: []const u8 = undefined;
-        var tmp: [3]u8 = undefined;
+        var tmp: [3]u8 = .{ 0, 0, 0 };
 
         var b0: u8 = undefined;
         var b1: u8 = undefined;
@@ -40,7 +61,6 @@ const Encoder = struct {
         var e3: u6 = undefined;
 
         while (i < input.len) : (i += 3) {
-            tmp = .{ 0, 0, 0 };
             var count: usize = 0;
             if (i + 3 > input.len) {
                 var z: usize = 0;
@@ -97,6 +117,46 @@ const Encoder = struct {
 
         return try list.toOwnedSlice(allocator);
     }
+
+    pub fn decode(allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        var list = try std.ArrayList(u8).initCapacity(allocator, ((input.len + 3) / 4) * 3);
+
+        var count: usize = 0;
+        var tmp: [4]u8 = .{ 0, 0, 0, 0 };
+
+        var e0: u8 = undefined;
+        var e1: u8 = undefined;
+        var e2: u8 = undefined;
+
+        for (0..input.len) |i| {
+            tmp[count] = try _base64.index_from_char(input[i]);
+            count += 1;
+
+            // end of sequence
+            if (count == 4) {
+                e0 = (tmp[0] << 2) | (tmp[1] >> 4);
+                list.appendAssumeCapacity(e0);
+
+                if (tmp[2] != 64) {
+                    e1 = (tmp[1] << 4) | (tmp[2] >> 2);
+                    list.appendAssumeCapacity(e1);
+                }
+
+                if (tmp[3] != 64) {
+                    e2 = (tmp[2] << 6) | tmp[3];
+                    list.appendAssumeCapacity(e2);
+                }
+
+                count = 0;
+            }
+        }
+
+        return try list.toOwnedSlice(allocator);
+    }
 };
 
 pub fn main(init: std.process.Init) !void {
@@ -123,10 +183,13 @@ pub fn main(init: std.process.Init) !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const base64_message = try Encoder.encode(allocator, &[_]u8{ 'H', 'i' });
+    const base64_message = try Encoder.encode(allocator, &[_]u8{'H'});
     defer allocator.free(base64_message);
-
     try stdout_writer.print("encoded message: {s}\n", .{base64_message});
+
+    const decoded = try Encoder.decode(allocator, &[_]u8{ 'S', 'A', '=', '=' });
+    defer allocator.free(decoded);
+    try stdout_writer.print("decoded message: {s}\n", .{decoded});
 
     try stdout_writer.flush(); // Don't forget to flush!
 }
